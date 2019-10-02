090f6abed17f1d83c3668c4949443d93026cfcec
ganba <gnbtr02@yahoo.com>
2019-10-02 02:17:18 -0400

    2nd round of changes in Mixed Model

--- R/mixed.R
+++ R/mixed.R
@@ -10,26 +10,32 @@
 #' @export
 #'
 #' @examples
-gce_mult <- function(Y, X, dimV, nu, p0, w0, optim_method = "BFGS") {
-  dimX <- dim(X)
-  N <- dimX[1]
-  K <- dimX[2]
-  J <- dim(Y)[2]
-  if (missing(dimV)) {
-    M <- 5
-  } else {
-    M <- dimV
-  }
-  v <- matrix(seq(from = -1, to = 1, length.out = M), nrow = 1)
-  if (missing(nu)) nu <- 0.5
-  if (missing(p0)) p0 <- matrix(1 / J, N, J)
-  if (missing(w0)) w0 <- array(1 / M, c(N, J, M))
+gme_mixed <- function(Y, X1, X2, dimS, dimV, optim_method = "BFGS") {
+  dY <- dim(Y)
+  N  <- dY[1]
+  J  <- dY[2]
+  K1 <- dim(X1)[2]
+  K2 <- dim(X2)[3]
 
-  lambda0 <- rep(0, K * (J - 1))
-  gce_optim <- optim(lambda0, gce_mult_obj, gce_mult_grad,
-                     Y = Y, X = X, v = v, nu = nu, p0 = p0,
-                     w0 = w0, N = N, K = K, J = J, M = M,
-                     method = optim_method)
+  if (missing(dimS)) {
+    M <- 3
+  } else {
+    M <- dimS
+  }
+  s <- matrix(seq(from = -1, to = 1, length.out = M), nrow = 1)
+
+  if (missing(dimV)) {
+    H <- 5
+  } else {
+    H <- dimV
+  }
+  v <- matrix(seq(from = -1, to = 1, length.out = H), nrow = 1)
+
+  param0 <- rep(0, (K1 * (J - 1) + K2 + N))
+  gce_optim <- optim(param0, gme_mixed_obj, gme_mixed_grad,
+                     Y = Y, X1 = X1, X2 = X2, s = s, v = v,
+                     N = N, J = J, K1 = K1, K2 = K2, M = M,
+                     H = H, method = optim_method)
   lambda_hat <- matrix(gce_optim$par, K, J - 1)
   lambda_hat <- cbind(rep(0, K), lambda_hat)
   gce_all <- list("par" = lambda_hat, "convergence" = gce_optim$convergence,
@@ -98,19 +104,28 @@ summary_mixed <- function(gme_mult_results) {
   return(est_sum_all)
 }
 
-.gce_mult_obj <- function(param, Y, X, v, nu, N, K, J, M, H) {
+.gme_mixed_obj <- function(param, Y, X1, X2, s, v, N, J, K1, K2, M, H) {
 
-  lambda <- param[1:K * (J - 1)]
-  lambda <- matrix(lambda, K, (J - 1))
-  lambda <- cbind(rep(0, K), lambda)
-  rho    <- param[-1:K * (J - 1)]
+  lambda <- param[1:K1 * (J - 1)]
+  lambda <- matrix(lambda, K1, (J - 1))
+  lambda <- cbind(rep(0, K1), lambda)
+  alpha  <- param((K1 * (J - 1) + 1):(K1 * (J - 1) + K2))
+  rho    <- param[-1:(K1 * (J - 1) + K2)]
 
-  phi   <- array(0, c(N, J, M))
-  Omega <- matrix(0, N, J)
+  temp1  <- X1 %*% lambda
+  temp2  <- matrix(0, N, J)
+  for (i in 1:N) {
+    for (j in 1:J) {
+      temp2[i, j] <- sum(alpha * X[i, j, ])
+    }
+  }
+
+  phi    <- array(0, c(N, J, M))
+  Omega  <- matrix(0, N, J)
   for (i in 1:N) {
     for (j in 1:J) {
       for (m in 1:M) {
-        phi[i, j, m] <- exp(s[m] * (-sum(lambda[, j] * x[i, j, ]) - rho[i]))
+        phi[i, j, m] <- exp(s[m] * (- temp1[i, j] - temp2[i, j] - rho[i]))
       }
       Omega[i, j] <- sum(phi[i, j, ])
     }
@@ -121,52 +136,86 @@ summary_mixed <- function(gme_mult_results) {
   for (i in 1:N) {
     for (j in 1:J) {
       for (h in 1:H) {
-        w[i, j, h] <- exp(-v[h] * sum(lambda[, j] * x[i, j, ]))
+        w[i, j, h] <- exp(v[h] * (- temp1[i, j] - temp2[i, j]))
       }
-      Psi[i, j] <- sum(w[i, j, h])
+      Psi[i, j] <- sum(w[i, j, ])
     }
   }
 
-  like  <- sum(lambda * (t(X) %*% Y)) + (1 - nu) * sum(log(Omega)) + nu * sum(log(Psi))
+  temp3 <- rep(0, K2)
+  for (k in 1:K2) {
+    temp3[k] <- sum(Y * X2[, , k])
+  }
+  like  <- sum(lambda * (t(X1) %*% Y)) + sum(alpha * temp3) + sum(log(Omega)) + sum(log(Psi))
   return(like)
 
 }
 
-.gce_mult_grad <- function(lambda_vector, Y, X, v, nu, p0, w0, N, K, J, M) {
+.gme_mixed_grad <- function(param, Y, X1, X2, s, v, N, J, K1, K2, M, H) {
 
-  lambda <- matrix(lambda_vector, K, (J - 1))
-  lambda <- cbind(rep(0, K), lambda)
+  lambda <- param[1:K1 * (J - 1)]
+  lambda <- matrix(lambda, K1, (J - 1))
+  lambda <- cbind(rep(0, K1), lambda)
+  alpha  <- param((K1 * (J - 1) + 1):(K1 * (J - 1) + K2))
+  rho    <- param[-1:(K1 * (J - 1) + K2)]
 
-  temp  <- X %*% lambda
-  p <- p0 * exp(temp / (1 - nu))
-  Omega <- apply(p, 1, sum)
-  for (i in 1:N) {
-    p[i, ] <- p[i, ] / Omega[i]
-  }
-
-  w   <- array(0, c(N, J, M))
-  Psi <- matrix(0, N, J)
-  e   <- matrix(0, N, J)
+  temp1  <- X1 %*% lambda
+  temp2  <- matrix(0, N, J)
   for (i in 1:N) {
     for (j in 1:J) {
-      w[i, j, ] <- w0[i, j, ] * exp(temp[i, j] * v / nu)
-      Psi[i, j] <- sum(w[i, j, ])
-      w[i, j, ] <- w[i, j, ] / Psi[i, j]
-      e[i, j]   <- sum(as.vector(v) * w[i, j, ])
+      temp2[i, j] <- sum(alpha * X2[i, j, ])
     }
   }
 
-  dlambda <- rep(0, K * (J -1) )
-  index_lambda <- 1
-  for (j in 2:J) {
-    for (k in 1:K) {
-      dlambda[index_lambda] <- - sum(Y[, j] * X[, k]) +
-        sum(p[, j] * X[, k]) +
-        sum(e[, j] * X[, k])
-      index_lambda <- index_lambda + 1
+  phi    <- array(0, c(N, J, M))
+  p      <- matrix(0, N, J)
+  Omega  <- matrix(0, N, J)
+  for (i in 1:N) {
+    for (j in 1:J) {
+      for (m in 1:M) {
+        phi[i, j, m] <- exp(s[m] * (- temp1[i, j] - temp2[i, j] - rho[i]))
+      }
+      Omega[i, j] <- sum(phi[i, j, ])
+      phi[i, j, ] <- phi[i, j, ] / Omega[i, j]
+      p[i, j]     <- sum(s * phi[i, j, ])
     }
   }
-  return(dlambda)
+
+  w   <- array(0, c(N, J, H))
+  e   <- matrix(0, N, J)
+  Psi <- matrix(0, N, J)
+  for (i in 1:N) {
+    for (j in 1:J) {
+      for (h in 1:H) {
+        w[i, j, h] <- exp(v[h] * (- temp1[i, j] - temp2[i, j]))
+      }
+      Psi[i, j] <- sum(w[i, j, ])
+      w[i, j, ] <- w[i, j, ] / Psi[i, j]
+      e[i, j]   <- sum(v * w[i, j, ])
+    }
+  }
+
+  dparam <- rep(0, (K1 * (J -1) + K2 + N))
+  index_param <- 1
+  for (j in 2:J) {
+    for (k in 1:K1) {
+      dparam[index_param] <- sum(Y[, j] * X1[, k]) -
+        sum(p[, j] * X1[, k]) -
+        sum(e[, j] * X1[, k])
+      index_param <- index_param + 1
+    }
+  }
+
+  for (k in 1:K2) {
+    dparam[index_param] <- sum(Y * X2[, , k]) - sum(p * X2[, , k]) - sum(e * X2[, , k])
+    index_param <- index_param + 1
+  }
+
+  for (i in 1:N) {
+    dparam[index_param] <- 1 - sum(p[i, ])
+    index_param <- index_param + 1
+  }
+  return(dparam)
 
 }
 
